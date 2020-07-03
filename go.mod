4ccf951c7e7a5ba5713dd4acf28808ee9c0fa05a
Adith A Danthi <adith.danthi@gmail.com>
2020-07-03 21:48:41 +0530

    added mux router, home endpoint handler

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module Go-MoviesAPI-Sample
 
 go 1.14
+
+require (
+	github.com/gorilla/mux v1.7.4 // indirect
+	go.mongodb.org/mongo-driver v1.3.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"net/http"
+)
+
+type Movie struct {
+	ID          primitive.ObjectID `bson:"_id" json:"id"`
+	Name        string             `bson:"name" json:"name"`
+	Description string             `bson:"description" json:"description"`
+	CoverImage  string             `bson:"cover_image" json:"cover_image"`
+}
+
+var coll *mongo.Collection
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Movies Home Page")
+}
+
+func handleRequests() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", homeHandler)
+
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+	client, _ := mongo.Connect(context.TODO(), clientOptions)
+	coll = client.Database("moviesdb").Collection("movies")
+	defer client.Disconnect(context.Background())
+	fmt.Println("Application started Successfully")
+
+	handleRequests()
+
 }
